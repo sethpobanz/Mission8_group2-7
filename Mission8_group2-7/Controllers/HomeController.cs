1b6289e9f2510aeaaf0b2a00ef85a233960d7d9c
sethpobanz <sethpobanz@gmail.com>
2024-02-28 09:17:26 -0700

    removed error controller

--- Mission8_group2-7/Controllers/HomeController.cs
+++ Mission8_group2-7/Controllers/HomeController.cs
@@ -67,10 +67,6 @@ namespace Mission8_group2_7.Controllers
 
         
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+     
     }
 }
